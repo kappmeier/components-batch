63edb8117365616ee12829376fae098fcb4fe3f1
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2015-04-09 21:14:58 +0200

    batch algorithm tweaks

---
+++ src/org/zetool/components/batch/plugins/AlgorithmicOperation.java
@@ -0,0 +1,32 @@
+
+package org.zetool.components.batch.plugins;
+
+import org.zetool.common.algorithm.Algorithm;
+
+
+/**
+ *
+ * @author Jan-Philipp Kappmeier
+ */
+public interface AlgorithmicOperation<U,V> {
+	public String getName();
+
+	public Class<U> accepts();
+
+
+	public Class<V> generates();
+
+	public default boolean canTake( Class<?> type ) {
+		return type == accepts();
+	}
+
+	public default boolean canGenerate( Class<?> type ) {
+		return type == generates();
+	}
+
+	/**
+	 * Factory method generating an actual algorithm of the plugins type.
+	 * @return an {@link Algorithm} instance
+	 */
+	public Algorithm<U,V> getAlgorithm();
+}
