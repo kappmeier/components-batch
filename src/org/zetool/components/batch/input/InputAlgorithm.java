98a86572e7ec993f63ceab1a4e9927c464817579
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2015-06-19 22:13:16 +0200

    Typos

--- src/org/zetool/components/batch/input/InputAlgorithm.java
+++ src/org/zetool/components/batch/input/InputAlgorithm.java
@@ -1,10 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.zetool.components.batch.input;
 
 import org.zetool.common.algorithm.Algorithm;
+
 
 /**
  *
